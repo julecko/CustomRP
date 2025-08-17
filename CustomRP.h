b474b7316153a3f68a2fcb96f66dd14467beb3ec
Julecko <julecko@dilino.sk>
2025-08-17 15:35:09 +0200

    Initial commit

---
+++ CustomRP.h
@@ -0,0 +1,32 @@
+
+// CustomRP.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CCustomRPApp:
+// See CustomRP.cpp for the implementation of this class
+//
+
+class CCustomRPApp : public CWinApp
+{
+public:
+	CCustomRPApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CCustomRPApp theApp;
---
+++ Dlg.cpp
@@ -0,0 +1,103 @@
+
+// Dlg.cpp : implementation file
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "CustomRP.h"
+#include "Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// Dlg dialog
+
+
+
+Dlg::Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_CUSTOMRP_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(Dlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_WM_SYSCOMMAND()
+END_MESSAGE_MAP()
+
+
+// Dlg message handlers
+
+BOOL Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// Set the icon for this dialog.  The framework does this automatically
+	//  when the application's main window is not a dialog
+	SetIcon(m_hIcon, TRUE);			// Set big icon
+	SetIcon(m_hIcon, FALSE);		// Set small icon
+
+	LONG style = ::GetWindowLong(this->m_hWnd, GWL_STYLE);
+	style |= WS_SYSMENU | WS_MINIMIZEBOX;
+	::SetWindowLong(this->m_hWnd, GWL_STYLE, style);
+	::SetWindowPos(this->m_hWnd, nullptr, 0, 0, 0, 0,
+		SWP_NOZORDER | SWP_NOMOVE | SWP_NOSIZE | SWP_FRAMECHANGED);
+
+	return TRUE;  // return TRUE  unless you set the focus to a control
+}
+
+// If you add a minimize button to your dialog, you will need the code below
+//  to draw the icon.  For MFC applications using the document/view model,
+//  this is automatically done for you by the framework.
+
+void Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // device context for painting
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// Center icon in client rectangle
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// Draw the icon
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// The system calls this function to obtain the cursor to display while the user drags
+//  the minimized window.
+HCURSOR Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+void Dlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	switch (nID)
+	{
+	case SC_CLOSE: EndDialog(IDOK); break;
+	case SC_MINIMIZE: ShowWindow(SW_MINIMIZE); break;
+	case SC_MAXIMIZE: ShowWindow(SW_MAXIMIZE); break;
+	default: CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
